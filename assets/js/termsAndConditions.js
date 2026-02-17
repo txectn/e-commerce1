feaea7699c896be175dd8571eeab57d079a784ce
txectn <nahid32277@gmail.com>
2026-02-17 19:34:17 +0600

    first commit

---
+++ assets/js/termsAndConditions.js
@@ -0,0 +1,8 @@
+/* ================================================================== 
+STRUCTURE & RESPONSIBILITIES
+- help topics dropdown start - help topics dropdown end
+- help topics switch start - help topics switch end
+==================================================================== */
+
+
+
